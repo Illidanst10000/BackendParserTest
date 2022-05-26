43cd6fcc3a8696c6b167136db6524db7280ca4fe
illidanst10000 <illidanst10000@gmail.com>
2022-05-26 18:29:25 +0300

    Some refactoring parser

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -12,37 +12,37 @@ class ParserController extends Controller
     public function __invoke()
     {
         $input = file_get_contents('https://lifehacker.com/rss');
-
         $data = simplexml_load_string($input, 'SimpleXMLElement', LIBXML_NOCDATA);
 
         $this->parsePosts($data);
-
     }
 
     public function parsePosts($data)
     {
-        foreach ($data->channel->item as $value) {
+        foreach ($data->channel->item as $value)
+        {
             $postValue = (array)$value;
 
-            if (array_key_exists('category', $postValue)) {
+            if (array_key_exists('category', $postValue))
+            {
                 $postCategories = $postValue['category'];
                 unset($postValue['category']);
             }
 
-            $postValue['pubDate'] = (new \DateTime())->createFromFormat('D, d M Y H:i:s \G\M\T', $postValue['pubDate']);
+            $postValue['pubDate'] = (new \DateTime())
+                ->createFromFormat('D, d M Y H:i:s \G\M\T', $postValue['pubDate']);
 
             $post = Post::firstOrCreate($postValue);
 
-            if (isset($postCategories)) {
+            if (isset($postCategories))
+            {
                 $categoryIds = $this->parseCategory($postCategories);
-
-                foreach ($categoryIds as $categoryId) {
-                    $post->categories()->attach($categoryId);
-                }
-            } else {
-                $post->categories()->delete();
+                $this->attachCategory($post, $categoryIds);
             }
-
+                else
+                {
+                    $post->categories()->delete();
+                }
         }
 
         dd("completed");
@@ -50,12 +50,22 @@ class ParserController extends Controller
 
     public function parseCategory($postCategories)
     {
-        foreach ($postCategories as $categoryTitle) {
+        foreach ($postCategories as $categoryTitle)
+        {
             $categoryData = ['title' => $categoryTitle];
             $category = Category::firstOrCreate($categoryData);
+
             $categoryIds[] = $category->id;
         }
         return $categoryIds;
+    }
+
+    public function attachCategory($post, $categoryIds)
+    {
+        foreach ($categoryIds as $categoryId)
+        {
+            $post->categories()->attach($categoryId);
+        }
     }
 
 }
